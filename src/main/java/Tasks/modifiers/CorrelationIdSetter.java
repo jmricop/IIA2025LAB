23d9ffb01b3e0689f58c772ad96722ddaaafacc8
Daniel <Daniel@DESKTOP-367DR6M.home>
2025-11-07 21:50:26 +0100

    Translator y ContextSlimmer hechos CorrelatorIdSetter hecho

--- src/main/java/Tasks/modifiers/CorrelationIdSetter.java
+++ src/main/java/Tasks/modifiers/CorrelationIdSetter.java
@@ -4,10 +4,43 @@
  */
 package Tasks.modifiers;
 
+import Tasks.Task;
+import Tasks.taskEnum;
+import common.Message;
+import common.Slot;
+import java.util.ArrayList;
+
 /**
  *
  * @author apolo
  */
-public class CorrelationIdSetter {
+public class CorrelationIdSetter extends Task {
+
+    private int correlatorId;
+
+    public CorrelationIdSetter(taskEnum t, ArrayList<Slot> entrySlots, ArrayList<Slot> exitSlots) {
+        super(t, entrySlots, exitSlots);
+        correlatorId=1;
+    }
+    
+    
+    @Override
+    public void action() {
+        if (!isEmpty(0)) {
+            Message mensaje = getEntryMessage(0);
+            
+            correlatorId++;
+            mensaje.setIdDocument(correlatorId);
+            setMensajeSalida(mensaje, 0);
+
+        } else {
+            System.out.println("CorrelatorIdSetter: No hay mensajes en la cola de entrada");
+        }
+    }
+
+    @Override
+    public void mock() {
+        System.out.println("Correlator ID asignado: " + correlatorId);
+    }
     
 }
