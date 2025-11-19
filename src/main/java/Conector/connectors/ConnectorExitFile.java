99b7dcd63d7690188ac02a3adfbed28830d6a1f0
PabloGomez03 <jbarea155@gmail.com>
2025-11-19 16:31:39 +0100

    Connectors de Fichero finalizados

---
+++ src/main/java/Conector/connectors/ConnectorExitFile.java
@@ -0,0 +1,81 @@
+package Conector.connectors;
+
+import Conector.Conector;
+import Port.Port;
+import Port.ports.ExitPort; // Asegúrate de tener esta clase o usar Port genérico
+import common.Slot; // Importa tu clase Slot
+import common.Message; // Importa tu clase Message
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+
+public class ConnectorExitFile extends Conector {
+
+    private final String outputDirectory;
+    private final String filePrefix;
+
+   
+    public ConnectorExitFile(Port port,String outputDirectory, String filePrefix) {
+        
+        super(port);
+        Queue<Message> cola = new LinkedList<>();
+        Slot slotInterno = new Slot(cola);
+        
+        
+        this.port = new ExitPort(slotInterno);
+
+        
+        this.outputDirectory = outputDirectory;
+        this.filePrefix = filePrefix;
+
+       
+        new File(outputDirectory).mkdirs();
+    }
+
+    @Override
+    public void action() {
+        
+        if (this.port.getBuffer().isEmpty()) {
+            return;
+        }
+
+        try {
+            
+            Message msg = this.port.getBuffer().getFirstMessage(); 
+            
+            if (msg == null || msg.getDocument() == null) return;
+
+            
+            String fileName = filePrefix + System.currentTimeMillis() + ".xml";
+            File destFile = new File(outputDirectory, fileName);
+
+            
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            
+            
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+            
+            DOMSource source = new DOMSource(msg.getDocument());
+            StreamResult result = new StreamResult(destFile);
+
+            transformer.transform(source, result);
+
+            System.out.println("ConnectorExit: Archivo guardado exitosamente en " + destFile.getAbsolutePath());
+
+        } catch (TransformerException e) {
+            Logger.getLogger(ConnectorExitFile.class.getName()).log(Level.SEVERE, "Error guardando XML", e);
+        } catch (IllegalArgumentException e) {
+            Logger.getLogger(ConnectorExitFile.class.getName()).log(Level.SEVERE, "Error en ConnectorExitFile", e);
+        }
+    }
+}
